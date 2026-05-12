af2826a1286154059d82ae0b0bc8f08c8c1aec33
Abdelrhman Mohamed <abd0001102002@gmail.com>
2026-05-12 10:19:29 +0000

    x

--- MERN_PROJECT_FRONTEND/src/pages/AddUser.js
+++ MERN_PROJECT_FRONTEND/src/pages/AddUser.js
@@ -25,10 +25,9 @@ const AddUser = () => {
       formData.append("image", data.image);
       formData.append("name", data.name);
 
-	const test = "http://172.20.10.6:30500";
 	    console.log(process.env);
 
-      const res = await fetch(`${test}/user`, {
+      const res = await fetch(`${process.env.REACT_APP_SERVER_URL}/user`, {
         method: "POST",
         body: formData,
       });
