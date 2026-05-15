32504590e81a926bad0abbecf8b429eb6a013610
Abdelrhman Mohamed <abd0001102002@gmail.com>
2026-05-15 18:20:14 +0000

    update package.json

--- MERN_PROJECT_FRONTEND/src/tests/App.test.js
+++ MERN_PROJECT_FRONTEND/src/tests/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import Navbar from "../components/Navbar";
 import { MemoryRouter } from "react-router-dom";
-import App from "../App";
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
@@ -9,14 +9,22 @@ global.fetch = jest.fn(() =>
   })
 );
 
-describe("App Component", () => {
-  test("renders CRUD title in navbar", () => {
-    render(<MemoryRouter><App /></MemoryRouter>);
+describe("Navbar Component", () => {
+  test("renders CRUD title", () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
     expect(screen.getByText("CRUD")).toBeInTheDocument();
   });
 
   test("renders Add User link", () => {
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
     expect(screen.getByText("Add User")).toBeInTheDocument();
   });
 });
